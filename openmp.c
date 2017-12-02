cb5d81a80fd5c9f9c8dadb59661e174fe0f7285d
blackhawkn7 <jdp99@case.edu>
2017-12-02 17:40:38 -0500

    openmp basically done, timing, output needs fined tuned

--- openmp.c
+++ openmp.c
@@ -1,5 +1,3 @@
-//Jim Patrizi
-//EECS 338 Final
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h> // For atoi()
@@ -7,22 +5,6 @@
 #include <time.h>
 
 struct timeval start_time, stop_time, elapsed_time;  // timers
-
-void merge(int arr[], int l, int m, int r);
-
-
-// l is for left index and r is right index of the sub-array of arr to be sorted
-void mergeSort(int arr[], int l, int r)
-{
-   if (l < r)
-   {
-      int middle = (l+r)/2;
-      mergeSort(arr, l, middle);
-      mergeSort(arr, middle+1, r);
-      merge(arr, l, middle, r);
-   }
-}
-
 
 //Merge two halves of the original array
 void merge(int arr[], int l, int m, int r)
@@ -82,6 +64,33 @@ void merge(int arr[], int l, int m, int r)
     free(R);
 }
 
+void mergeSort(int arr[], int l, int r)
+{
+  if(l < r)
+  {
+      printf("Thread %d is sorting %d through %d\n", omp_get_thread_num(), l, r);
+      int middle = (l + r) / 2;
+
+      //have both halves go in parallel
+      #pragma omp parallel
+      {
+          //identifies a section of code that must be run by a single available thread
+          #pragma omp single
+          {
+              //used to execute this region of code in parallel with code outside of this task region
+              //ergo sort both halves of the array in parallel, only once!
+              #pragma omp task
+              mergeSort(arr, l, middle);
+
+              mergeSort(arr, middle + 1, r);
+          }
+      }
+
+      /* merge the two halves */
+      merge(arr, l, middle, r);
+  }
+}
+
 int *randomish_array(int n) {
     srand(time(NULL));
     int *rand_arr = (int *)malloc(n * sizeof(int));
@@ -94,32 +103,36 @@ int *randomish_array(int n) {
     return rand_arr;
 }
 
-//Test finctions
-int main(int argc, char **argv)
-{
-    if(argc != 2)
+int main(int argc, char **argv) {
+    if(argc != 3)
     {
-      printf("Usage is ./a.out <# of random numbers to Sort>\n");
+      printf("Usage is ./a.out <# of random numbers to Sort> <# of threads>\n")
       return 0;
     }
+    int i;
     int n = atoi(argv[1]);
+    int threadNum = atoi(argv[2]);
+    omp_set_num_threads(threadNum);
     int *p = randomish_array(n);
     if (!p)
     {
         return 1;
     }
-    //int size = sizeof(elementArray)/sizeof(elementArray[0]);
+
+    //enable recursive parallel blocks
+    omp_set_nested(1);
+
     gettimeofday(&start_time,NULL); // Unix timer
     mergeSort(p, 0, n - 1);
     gettimeofday(&stop_time,NULL);
-	  timersub(&stop_time, &start_time, &elapsed_time); // Unix time subtract routine
-	  printf("Total time was %f seconds.\n", elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0);
-    printf("\nSorted array is \n");
-    int i;
-    // for(i = 0; i < n; i++)
-    // {
-    //   printf("%d\n",p[i]);
-    // }
+    timersub(&stop_time, &start_time, &elapsed_time); // Unix time subtract routine
+    /* print them again */
+    printf("\nSorted numbers: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", p[i]);
+    }
+    printf("\n");
+    printf("Total time was %f seconds.\n", elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0);
     free(p);
     return 0;
 }
